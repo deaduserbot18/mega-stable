07b5c380ffa44886106e64d6abc533662f0ab49f
deaduserbot18 <lvaparna55@gmail.com>
2021-04-16 21:44:10 +0530

    Initial commit

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,24 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mega'
+        self.UnzipMirrorCommand = 'unzipmega'
+        self.TarMirrorCommand = 'tarmega'
+        self.CancelMirror = 'cancel'
+        self.CancelAllCommand = 'cancelall'
+        self.ListCommand = 'list'
+        self.SpeedCommand = 'speedtest'
+        self.StatusCommand = 'mstatus'
+        self.AuthorizeCommand = 'authorize'
+        self.UnAuthorizeCommand = 'unauthorize'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart'
+        self.StatsCommand = 'mstats'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.CloneCommand = "lol"
+        self.WatchCommand = 'watch'
+        self.TarWatchCommand = 'tarwatch'
+        self.deleteCommand = 'del'
+
+BotCommands = _BotCommands()
